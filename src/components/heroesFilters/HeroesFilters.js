e7e13e10ae94a615476b83d366a5219ceed2f9ca
Dima <dima.rastorguev.2016@mail.ru>
2024-08-09 02:14:16 +0300

    create project hero_admin_panel

---
+++ src/components/heroesFilters/HeroesFilters.js
@@ -0,0 +1,63 @@
+import { useHttp } from "../../hooks/http.hook";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import classNames from "classnames";
+
+import {filtersFetching, filtersFetched, filtersFetchingError, actionFilterChanged } from "../../actions";
+import Spinner from "../spinner/Spinner";
+
+const HeroesFilters = () => {
+
+    const {filters, filtersLoadingStatus, activeFilter} = useSelector(state => state);
+    const dispatch = useDispatch();
+    const {request} = useHttp();
+
+    useEffect(() => {
+        dispatch(filtersFetching())
+        request('http://localhost:3001/filters')
+            .then(data => dispatch(filtersFetched(data)))
+            .catch(() => dispatch(filtersFetchingError()))
+        // eslint-disable-next-line
+    }, []);
+
+    if (filtersLoadingStatus === "loading") {
+        return <h5 className="text-center mt-5"><Spinner/></h5>
+    } else if (filtersLoadingStatus === "error") {
+        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+    } // Если эти 2 условия не выполнились, то код идет дальше
+
+    const renderFilters = (arr) => {
+        if (arr.length === 0) {
+            return <h5 className="text-center mt-5">Фильтры не найдены</h5>
+        }
+
+        return arr.map(({name, label, className}) => {
+            const btnClass = classNames('btn', className, {
+                'active': name === activeFilter
+            })
+
+            return <button
+                        className={btnClass}
+                        key={name}
+                        id={name}
+                        onClick={() => dispatch(actionFilterChanged(name))}
+                        >{label}</button>
+        });
+    };
+    
+    
+
+    const elements = renderFilters(filters);
+    return (
+        <div className="card shadow-lg mt-4">
+            <div className="card-body">
+                <p className="card-text">Отфильтруйте героев по элементам</p>
+                <div className="btn-group">
+                    {elements}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroesFilters;
---
+++ src/components/heroesList/HeroesList.js
@@ -0,0 +1,73 @@
+import {useHttp} from '../../hooks/http.hook';
+import { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { CSSTransition, TransitionGroup} from 'react-transition-group';
+
+import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
+import HeroesListItem from "../heroesListItem/HeroesListItem";
+import Spinner from '../spinner/Spinner';
+import './heroesList.scss';
+
+const HeroesList = () => {
+    const filteredHeroes = useSelector(state => {
+        if (state.activeFilter === 'all') {
+            return state.heroes;
+        } else {
+            return state.heroes.filter(item => item.element === state.activeFilter)
+        }
+    });
+    const heroesLoadingStatus = useSelector(state => state.heroesLoadingStatus);
+    const dispatch = useDispatch();
+    const {request} = useHttp();
+
+    useEffect(() => {
+        dispatch(heroesFetching()); // запускаем загрузку
+        request("http://localhost:3001/heroes")
+            .then(data => dispatch(heroesFetched(data)))
+            .catch(() => dispatch(heroesFetchingError()))
+
+        // eslint-disable-next-line
+    }, []);
+
+    const onDelete = useCallback((id) => {
+        request(`http://localhost:3001/heroes/${id}`, 'DELETE')
+            .then((data) => console.log(data, 'delete'))
+            .then(dispatch(heroDeleted(id)))
+        // eslint-disable-next-line
+    }, [request]);
+
+    if (heroesLoadingStatus === "loading") {
+        return <Spinner/>;
+    } else if (heroesLoadingStatus === "error") {
+        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+    } // Если эти 2 условия не выполнились, то код идет дальше
+
+    const renderHeroesList = (arr) => {
+        if (arr.length === 0) {
+            return (
+                <CSSTransition in={true} timeout={500} classNames={'item'}>
+                    <h5 className="text-center mt-5">Героев пока нет</h5>
+                </CSSTransition>
+            )
+        }
+        console.log('1')
+        return arr.map(({id, ...props}) => {
+            return (
+                    <CSSTransition in={true} key={id} timeout={500} classNames={'item'}>
+                        <HeroesListItem key={id} {...props} onDelete={() => onDelete(id)}/>
+                    </CSSTransition>
+            );
+        });
+    }
+    
+    const elements = renderHeroesList(filteredHeroes);
+    return (
+        <ul>
+            <TransitionGroup component={null}>
+                {elements}
+            </TransitionGroup>
+        </ul>
+    )
+}
+
+export default HeroesList;
